5cf9b09442c63b464c185b8dc9fb29a987ccc2c6
Micah Pegman <wyntre5150@gmail.com>
2020-02-25 17:25:54 -0600

    Create Edit.vue

---
+++ src/components/characters/Edit.vue
@@ -0,0 +1,97 @@
+<template>
+  <b-container fluid>
+    <b-row class="form-row">
+      <b-col sm="12">
+        <b-alert v-model="show" class="mt-3" dismissible @dismissed="dismissed">
+          {{ name  }} updated!
+        </b-alert>
+      </b-col>
+    </b-row>
+    <b-row class="form-row">
+      <b-col sm="3">
+        <label>Name:</label>
+      </b-col>
+      <b-col sm="9">
+        <b-form-input lazy v-model="name"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row class="form-row">
+      <b-col sm="3">
+        <label>Description:</label>
+      </b-col>
+      <b-col sm="9">
+        <b-form-textarea v-model="description"></b-form-textarea>
+      </b-col>
+    </b-row>
+    <b-row class="form-create-row">
+      <b-col sm="12">
+        <b-button variant="success" @click="submitData">Update</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'CharactersEdit',
+  data () {
+    return {
+      name: '',
+      description: '',
+      show: false,
+      characters: []
+    }
+  },
+
+  created: function () {
+    this.fetchData()
+  },
+
+  methods: {
+    submitData: function() {
+      this.show = !this.show
+    },
+    dismissed: function () {
+      console.log('Alert dismissed')
+    },
+    fetchData: function () {
+      this.axios.get('/json/characters.json')
+        .then((response) => {
+          this.characters = response.data
+          this.displayData()
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
+    },
+    displayData: function () {
+      for (var i = 0; i < this.characters.length; i++) {
+        console.log('Searching characters...')
+        if (this.characters[i]['ID'] === this.$route.params.id) {
+          console.log('Found ' + this.characters[i]['Name'])
+          this.name = this.characters[i]['Name']
+          this.description = this.characters[i]['Description']
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-row {
+  padding: 15px;
+}
+
+.form-row div {
+  text-align: left;
+}
+
+.form-create-row {
+  padding: 15px;
+}
+
+.form-create-row div {
+  text-align: right;
+}
+</style>
