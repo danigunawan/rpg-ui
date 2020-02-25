d0f422ac53e658bf753b4fbf6957c21f37f2034d
Micah Pegman <wyntre5150@gmail.com>
2020-02-25 17:28:13 -0600

    Create List.vue

---
+++ src/views/characters/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div id="header" class="text-left">
+      <b-button
+        :to='{ name: "CharacterNew" }'
+        variant='primary'
+      >
+        Create
+      </b-button>
+    </div>
+    <CharactersList/>
+  </div>
+</template>
+
+<script>
+import CharactersList from '@/components/characters/List.vue'
+
+export default {
+  name: 'CharacterList',
+  components: {
+    CharactersList
+  }
+}
+</script>
+
+<style scoped>
+#header {
+  padding-bottom: 15px;
+}
+</style>
