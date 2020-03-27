0268fea4b2b98da621cd7e24a5bfde198573b343
Micah Pegman <wyntre5150@gmail.com>
2020-03-27 18:25:38 -0500

    Added campaign templates

---
+++ src/views/campaigns/List.vue
@@ -0,0 +1,14 @@
+const ModalWindow = () => ({
+  component: import('./ModalWindow.vue'),
+  loading: LoadingComponent,
+  error: ErrorComponent,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: 3000
+})
+
+export default {
+  components: {
+    ModalWindow
+  }
+}
