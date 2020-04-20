0b4f92b10cc4c98ce9b7af81ba77be4b3849b8ce
Micah Pegman <wyntre5150@gmail.com>
2020-04-20 13:18:52 -0500

    update for auth token to be used from localStorage

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -1,6 +1,5 @@
 export default function guest ({next, store}) {
-  if (!store.getters.auth.token || !localStorage.getItem("token")) {
-    store.mutations.clearToken
+  if (!localStorage.getItem("token")) {
     return next({
       name: 'Login'
     })
